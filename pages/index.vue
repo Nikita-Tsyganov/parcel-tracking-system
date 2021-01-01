c98d7123f84d3f588a793421756131409bc26850
Vazyri <Vazyri>
2021-01-01 15:26:23 -0600

    Index skeleton page, no functionality yet

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,69 @@
 <template>
   <div class="container">
-    <head>
-        <!-- responsive viewport meta -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <h1 class="title">Item Tracking</h1>
-    </head>
-  </div>
-  <div>
-    <form>
-	    <label for="TrackingID">Tracking ID</label>
-        <br /> 
-	    <textarea cols="45" rows="2"></textarea>
-        <br /> 
-	    <button type="submit" value="Search">Search</button>
+    <header class="header">
+        <h1>Parcel Tracker</h1>
+    </header>
+    <form class="form" @submit.prevent="addTrackingID">
+	  <input type="text" v-model="trackingID" name="trackingID" placeholder="Enter tracking ID">
+	  <input type="submit" value="Search" class="btn">
      </form>
   </div>
 </template>
-
 <script>
-export default {}
+export default {
+    name: "TrackingID",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTrackingID() {
+            const newTrackingID = {
+                title: this.title
+            }
+            // Send up to parent
+            this.$emit('add-tracking-id', newTrackingID);
+            
+            //clear field
+            this.title = '';
+        }
+    }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+    .header {
+        display: flex;
+        justify-content:center;
+        background: #732323;
+        color: #FFF;
+        padding: 10px;
+    }
+    .form {
+        display: flex;
+        flex-direction:column;
+        align-items:center;
+        padding: 10%; 
+    }
+    .container div {
+        border: 1px #ccc solid;
+        flex-basis: 25%; 
+    }
+    .input[type="text"] {
+        flex: 2;
+    }
+    .btn {
+        flex: 1;
+        background: #FF4D4D;
+        color: #FFF;
+        padding: 7px 20px;
+        cursor: pointer;
+    }
+    .btn:hover {
+        background: #FFF;
+    }
+    .links {
+      padding-top: 15px;
+    }
 </style>
