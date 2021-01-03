48b000475bb4c9ea13bf330e42b22ca060632c4a
Vazyri <Vazyri>
2021-01-02 22:49:33 -0600

    New bootstrap styling, new TrackingHistory page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="container">
-    <div class="tracking-id-input">
-        <!-- @submit, v-model, etc. for for js functionality, yet to be completed -->
-        <form class="form" @submit.prevent="addTrackingID">
-            <label class="mb-2" for="trackingID">Tracking numbers</label>
-            <b-form-input class="mb-2" type="text" v-model="trackingID" name="trackingID" placeholder="Enter up to 24 items, separated by commas or line breaks"></b-form-input>
-            <b-button squared variant="primary" class="mb-2" type="submit">Track</b-button>
-        </form>
+    <div class="container">
+        <div class="tracking-id-input">
+            <!-- @submit, v-model, etc. for for js functionality, yet to be completed -->
+            <form class="mt-5">
+                <label class="mb-2" for="trackingID">Tracking numbers</label>
+                <b-form-input class="mb-2" type="text" v-model="trackingID" name="trackingID" placeholder="Enter up to 24 items, separated by commas or line breaks"></b-form-input>
+                <b-button squared variant="primary" class="mb-2" type="submit">Track</b-button>
+            </form>
+        </div>
     </div>
-  </div>
 </template>
 
 <!-- not done -->
 <script>
+import TrackingHistory from "@/pages/TrackingHistory";
+
 export default {
     data() {
         return {
@@ -25,15 +27,9 @@ export default {
             const newTrackingID = {
                 title: this.title
             }
-            // Send up to parent
-            this.$emit('add-tracking-id', newTrackingID);
-            
             //clear field
             this.title = '';
         }
     }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
