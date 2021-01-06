c5a709f8e607c70b8b026365aad96ad2f822bac3
Vazyri <Vazyri>
2021-01-06 00:31:15 -0600

    Add _id folder with index.vue

---
+++ pages/_id/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{trackingID}}
+    </div>
+</template>
+
+<script>
+import trackingID from '@/pages/index';
+
+export default {
+    data() {
+        return {
+            trackingID,
+            progress: {}
+        };
+    },
+    //created() {
+    //    const parcel = axios
+    //    .get('parcels/${this.$route.params.id}')
+    //    .then((parcel) => console.log(parcel))
+    //}
+}
+
+</script>
+
+<style>
+</style>
