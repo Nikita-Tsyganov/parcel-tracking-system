2c228ffdfe7b4ead415fb73467a565086dad760a
Vazyri <Vazyri>
2021-01-06 00:30:10 -0600

    Add Progress in Components

---
+++ components/Progress.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="progress">
+        <p>{{ progress }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Progress",
+        props: ["progress"]
+    }
+</script>
+
+<style lang="scss" scoped>
+.progress {
+    paddings: 1rem;
+    border: 1px dotted #CCC;
+    margin: 1rem 0;
+}
+</style>
