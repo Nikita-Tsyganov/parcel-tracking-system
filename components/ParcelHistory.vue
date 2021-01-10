5c21c58f26e124d9a30e606510fad37479a523ee
Nikita-Tsyganov <nikita.s.tsyganov@gmail.com>
2021-01-09 22:00:19 -0600

    Add colon to the time

--- components/ParcelHistory.vue
+++ components/ParcelHistory.vue
@@ -2,10 +2,10 @@
   <b-container class="parcel-history">
     <b-row cols="3">
       <b-col>
-        <p>{{ $moment(parcelHistory.datetime).format("MMM. D") }}</p>
+        <p>{{ $moment(parcelHistory.datetime).format('MMM. D') }}</p>
       </b-col>
       <b-col>
-        <p>{{ $moment(parcelHistory.datetime).format("h mm a") }}</p>
+        <p>{{ $moment(parcelHistory.datetime).format('h:mm a') }}</p>
       </b-col>
       <b-col>
         <p>{{ parcelHistory.status.status }}</p>
