1fe59869c8d4b5f1095e5a546b90ffb454ebe40b
Vazyri <Vazyri>
2021-01-09 20:02:40 -0600

    Format parcelHistories, add keypress

--- components/ParcelHistory.vue
+++ components/ParcelHistory.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    <p>{{ parcelHistory }}</p>
-  </div>
+  <b-container class="parcel-history">
+    <b-row cols="3">
+      <b-col>
+        <p>{{ parseDate(parcelHistories.datetime) }}</p>
+      </b-col>
+      <b-col>
+        <p>{{ parcelHistories.dateTime }}</p>
+      </b-col>
+      <b-col>
+        <p>{{ parcelHistories.status.status }}</p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 export default {
   name: 'ParcelHistory',
   props: ['parcelHistory'],
+
+  methods: {
+    parseDate(date) {
+      var parsedDate = new Date(date).toString()
+      console.log(parsedDate)
+    },
+  },
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
