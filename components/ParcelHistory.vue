f211921055b3bb79a7ec9a1b9fddee8714484409
Vazyri <Vazyri>
2021-01-09 21:56:04 -0600

    Date and time format with moment

--- components/ParcelHistory.vue
+++ components/ParcelHistory.vue
@@ -2,10 +2,10 @@
   <b-container class="parcel-history">
     <b-row cols="3">
       <b-col>
-        <p>{{ parseDate(parcelHistory.datetime) }}</p>
+        <p>{{ $moment(parcelHistory.datetime).format("MMM. D") }}</p>
       </b-col>
       <b-col>
-        <p>{{ parcelHistory.datetime }}</p>
+        <p>{{ $moment(parcelHistory.datetime).format("h mm a") }}</p>
       </b-col>
       <b-col>
         <p>{{ parcelHistory.status.status }}</p>
@@ -18,12 +18,5 @@
 export default {
   name: 'ParcelHistory',
   props: ['parcelHistory'],
-
-  methods: {
-    parseDate(date) {
-      var parsedDate = new Date(date).toString()
-      console.log(parsedDate)
-    },
-  },
 }
 </script>
