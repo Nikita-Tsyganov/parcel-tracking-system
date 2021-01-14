8ab3201b43d613768917894da50b26d12243c6a0
Grace-Luu <grace.luu.01@gmail.com>
2021-01-13 21:24:09 -0600

    Add state management

---
+++ store/employees.js
@@ -0,0 +1,54 @@
+import EmployeeAPI from '~/utils/api/EmployeeAPI.js'
+
+// const employeeAPI = new EmployeeAPI(this.$axios)
+
+export const state = () => ({
+  employees: [],
+})
+
+export const getters = {
+  get: (state) => (id) => {
+    return state.employees.find((v) => v.id == id) || {}
+  },
+}
+
+export const mutations = {
+  ALL(state, records) {
+    state.employees = records
+  },
+  CREATE(state, record) {
+    state.employees.push(record)
+  },
+  UPDATE(state, updatedRecord) {
+    let recordToUpdate = state.employees.find((v) => v.id == updatedRecord.id)
+    let index = state.employees.indexOf(recordToUpdate)
+    state.employees.splice(index, 1, updatedRecord)
+  },
+  DELETE(state, id) {
+    let records = state.employees.filter((v) => v.id != id)
+    state.employees = records
+  },
+}
+
+export const actions = {
+  async all({ commit }) {
+    let allRecords = await employeeAPI.all()
+    commit('SET', allRecords)
+  },
+  async create({ commit }, record) {
+    let createdRecord = await employeeAPI.create(record)
+    commit('CREATE', createdRecord)
+    return createdRecord
+  },
+  async update({ commit }, id, body) {
+    let updatedRecord = await employeeAPI.update(id, body)
+    commit('UPDATE', updatedRecord)
+    return updatedRecord
+  },
+  async delete({ commit }, id) {
+    let deletedRecord = await employeeAPI.delete(id)
+    if (response.status == 200 || response.status == 204) {
+      commit('DELETE', deletedRecord.id)
+    }
+  },
+}
---
+++ store/parcels.js
@@ -0,0 +1,58 @@
+import ParcelAPI from '~/utils/api/ParcelAPI.js'
+
+// const parcelAPI = new ParcelAPI(this.$axios)
+
+export const state = () => ({
+  parcels: [],
+})
+
+export const getters = {
+  get: (state) => (id) => {
+    return state.parcels.find((v) => v.id == id) || {}
+  },
+}
+
+export const mutations = {
+  ALL(state, records) {
+    state.parcels = records
+  },
+  CREATE(state, record) {
+    state.parcels.push(record)
+  },
+  UPDATE(state, updatedRecord) {
+    let recordToUpdate = state.parcels.find((v) => v.id == updatedRecord.id)
+    let index = state.parcels.indexOf(recordToUpdate)
+    state.parcels.splice(index, 1, updatedRecord)
+  },
+  DELETE(state, id) {
+    let records = state.parcels.filter((v) => v.id != id)
+    state.parcels = records
+  },
+}
+
+export const actions = {
+  async all({ commit }) {
+    let allRecords = await parcelAPI.all()
+    commit('SET', allRecords)
+  },
+  async find({ commit }, id) {
+    let foundRecord = await parcelAPI.find(id)
+    return foundRecord
+  },
+  async create({ commit }, record) {
+    let createdRecord = await parcelAPI.create(record)
+    commit('CREATE', createdRecord)
+    return createdRecord
+  },
+  async update({ commit }, id, body) {
+    let updatedRecord = await parcelAPI.update(id, body)
+    commit('UPDATE', updatedRecord)
+    return updatedRecord
+  },
+  async delete({ commit }, id) {
+    let deletedRecord = await parcelAPI.delete(id)
+    if (response.status == 200 || response.status == 204) {
+      commit('DELETE', deletedRecord.id)
+    }
+  },
+}
