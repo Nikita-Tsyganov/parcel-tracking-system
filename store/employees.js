d602e70ef8a95b19e33479c6e689f93d6fcd6997
Nikita-Tsyganov <nikita.s.tsyganov@gmail.com>
2021-01-13 23:12:45 -0600

    Update store to use the new api utility

--- store/employees.js
+++ store/employees.js
@@ -1,30 +1,20 @@
-import EmployeeAPI from '~/utils/api/EmployeeAPI.js'
-
-// const employeeAPI = new EmployeeAPI(this.$axios)
-
 export const state = () => ({
   employees: [],
 })
 
 export const getters = {
-  get: (state) => (id) => {
-    return state.employees.find((v) => v.id == id) || {}
-  },
+  get: (state) => (id) => state.employees.find((v) => v.id == id) || {},
 }
 
 export const mutations = {
-  ALL(state, records) {
-    state.employees = records
-  },
-  CREATE(state, record) {
-    state.employees.push(record)
-  },
-  UPDATE(state, updatedRecord) {
+  ALL: (state, records) => (state.employees = records),
+  CREATE: (state, record) => state.employees.push(record),
+  UPDATE: (state, updatedRecord) => {
     let recordToUpdate = state.employees.find((v) => v.id == updatedRecord.id)
     let index = state.employees.indexOf(recordToUpdate)
     state.employees.splice(index, 1, updatedRecord)
   },
-  DELETE(state, id) {
+  DELETE: (state, id) => {
     let records = state.employees.filter((v) => v.id != id)
     state.employees = records
   },
@@ -32,21 +22,25 @@ export const mutations = {
 
 export const actions = {
   async all({ commit }) {
-    let allRecords = await employeeAPI.all()
+    let allRecords = await this.$Employee.all()
     commit('ALL', allRecords)
   },
+  async find({ commit }, id) {
+    let foundRecord = await this.$Employee.find(id)
+    return foundRecord
+  },
   async create({ commit }, record) {
-    let createdRecord = await employeeAPI.create(record)
+    let createdRecord = await this.$Employee.create(record)
     commit('CREATE', createdRecord)
     return createdRecord
   },
   async update({ commit }, id, body) {
-    let updatedRecord = await employeeAPI.update(id, body)
+    let updatedRecord = await this.$Employee.update(id, body)
     commit('UPDATE', updatedRecord)
     return updatedRecord
   },
   async delete({ commit }, id) {
-    let deletedRecord = await employeeAPI.delete(id)
+    let deletedRecord = await this.$Employee.delete(id)
     if (response.status == 200 || response.status == 204) {
       commit('DELETE', deletedRecord.id)
     }
--- store/parcels.js
+++ store/parcels.js
@@ -1,30 +1,20 @@
-import ParcelAPI from '~/utils/api/ParcelAPI.js'
-
-// const parcelAPI = new ParcelAPI(this.$axios)
-
 export const state = () => ({
   parcels: [],
 })
 
 export const getters = {
-  get: (state) => (id) => {
-    return state.parcels.find((v) => v.id == id) || {}
-  },
+  get: (state) => (id) => state.parcels.find((v) => v.id == id) || {},
 }
 
 export const mutations = {
-  ALL(state, records) {
-    state.parcels = records
-  },
-  CREATE(state, record) {
-    state.parcels.push(record)
-  },
-  UPDATE(state, updatedRecord) {
+  ALL: (state, records) => (state.parcels = records),
+  CREATE: (state, record) => state.parcels.push(record),
+  UPDATE: (state, updatedRecord) => {
     let recordToUpdate = state.parcels.find((v) => v.id == updatedRecord.id)
     let index = state.parcels.indexOf(recordToUpdate)
     state.parcels.splice(index, 1, updatedRecord)
   },
-  DELETE(state, id) {
+  DELETE: (state, id) => {
     let records = state.parcels.filter((v) => v.id != id)
     state.parcels = records
   },
@@ -32,25 +22,25 @@ export const mutations = {
 
 export const actions = {
   async all({ commit }) {
-    let allRecords = await parcelAPI.all()
+    let allRecords = await this.$Parcel.all()
     commit('ALL', allRecords)
   },
   async find({ commit }, id) {
-    let foundRecord = await parcelAPI.find(id)
+    let foundRecord = await this.$Parcel.find(id)
     return foundRecord
   },
   async create({ commit }, record) {
-    let createdRecord = await parcelAPI.create(record)
+    let createdRecord = await this.$Parcel.create(record)
     commit('CREATE', createdRecord)
     return createdRecord
   },
   async update({ commit }, id, body) {
-    let updatedRecord = await parcelAPI.update(id, body)
+    let updatedRecord = await this.$Parcel.update(id, body)
     commit('UPDATE', updatedRecord)
     return updatedRecord
   },
   async delete({ commit }, id) {
-    let deletedRecord = await parcelAPI.delete(id)
+    let deletedRecord = await this.$Parcel.delete(id)
     if (response.status == 200 || response.status == 204) {
       commit('DELETE', deletedRecord.id)
     }
