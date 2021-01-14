9e8771866bf5873f6ac9918349b6033c1cfc2a67
Nikita-Tsyganov <nikita.s.tsyganov@gmail.com>
2021-01-14 09:44:22 -0600

    Add abstraction layer to store

--- store/employees.js
+++ store/employees.js
@@ -1,48 +1,14 @@
-export const state = () => ({
-  employees: [],
-})
+import {
+  baseState,
+  baseGetters,
+  baseMutations,
+  baseActions,
+} from '../utils/store.js'
 
-export const getters = {
-  get: (state) => (id) => state.employees.find((v) => v.id == id) || {},
-}
+export const state = baseState('employees')
 
-export const mutations = {
-  ALL: (state, records) => (state.employees = records),
-  CREATE: (state, record) => state.employees.push(record),
-  UPDATE: (state, updatedRecord) => {
-    let recordToUpdate = state.employees.find((v) => v.id == updatedRecord.id)
-    let index = state.employees.indexOf(recordToUpdate)
-    state.employees.splice(index, 1, updatedRecord)
-  },
-  DELETE: (state, id) => {
-    let records = state.employees.filter((v) => v.id != id)
-    state.employees = records
-  },
-}
+export const getters = baseGetters('employees')
 
-export const actions = {
-  async all({ commit }) {
-    let allRecords = await this.$Employee.all()
-    commit('ALL', allRecords)
-  },
-  async find({ commit }, id) {
-    let foundRecord = await this.$Employee.find(id)
-    return foundRecord
-  },
-  async create({ commit }, record) {
-    let createdRecord = await this.$Employee.create(record)
-    commit('CREATE', createdRecord)
-    return createdRecord
-  },
-  async update({ commit }, id, body) {
-    let updatedRecord = await this.$Employee.update(id, body)
-    commit('UPDATE', updatedRecord)
-    return updatedRecord
-  },
-  async delete({ commit }, id) {
-    let deletedRecord = await this.$Employee.delete(id)
-    if (response.status == 200 || response.status == 204) {
-      commit('DELETE', deletedRecord.id)
-    }
-  },
-}
+export const mutations = baseMutations('employees')
+
+export const actions = baseActions('$Employee')
--- store/parcels.js
+++ store/parcels.js
@@ -1,48 +1,14 @@
-export const state = () => ({
-  parcels: [],
-})
+import {
+  baseState,
+  baseGetters,
+  baseMutations,
+  baseActions,
+} from '../utils/store.js'
 
-export const getters = {
-  get: (state) => (id) => state.parcels.find((v) => v.id == id) || {},
-}
+export const state = baseState('parcels')
 
-export const mutations = {
-  ALL: (state, records) => (state.parcels = records),
-  CREATE: (state, record) => state.parcels.push(record),
-  UPDATE: (state, updatedRecord) => {
-    let recordToUpdate = state.parcels.find((v) => v.id == updatedRecord.id)
-    let index = state.parcels.indexOf(recordToUpdate)
-    state.parcels.splice(index, 1, updatedRecord)
-  },
-  DELETE: (state, id) => {
-    let records = state.parcels.filter((v) => v.id != id)
-    state.parcels = records
-  },
-}
+export const getters = baseGetters('parcels')
 
-export const actions = {
-  async all({ commit }) {
-    let allRecords = await this.$Parcel.all()
-    commit('ALL', allRecords)
-  },
-  async find({ commit }, id) {
-    let foundRecord = await this.$Parcel.find(id)
-    return foundRecord
-  },
-  async create({ commit }, record) {
-    let createdRecord = await this.$Parcel.create(record)
-    commit('CREATE', createdRecord)
-    return createdRecord
-  },
-  async update({ commit }, id, body) {
-    let updatedRecord = await this.$Parcel.update(id, body)
-    commit('UPDATE', updatedRecord)
-    return updatedRecord
-  },
-  async delete({ commit }, id) {
-    let deletedRecord = await this.$Parcel.delete(id)
-    if (response.status == 200 || response.status == 204) {
-      commit('DELETE', deletedRecord.id)
-    }
-  },
-}
+export const mutations = baseMutations('parcels')
+
+export const actions = baseActions('$Parcel')
