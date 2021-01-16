dd03363371819164a4e750614b6cfb846de44c62
Nikita-Tsyganov <nikita.s.tsyganov@gmail.com>
2021-01-15 20:16:11 -0600

    Add spacing.css to nuxt css config

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~assets/css/typography.css'],
+  css: ['~assets/css/typography.css', '~assets/css/spacing.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['~/plugins/models.js'],
