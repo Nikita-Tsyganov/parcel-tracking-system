353388c6a5da46d141b4b19b87e0ec980034fbf6
Nikita-Tsyganov <nikita.s.tsyganov@gmail.com>
2021-01-20 17:38:23 -0600

    Update Controller logic

--- server/controllers/ParcelController.js
+++ server/controllers/ParcelController.js
@@ -4,11 +4,8 @@ const { Parcel, ParcelEvent, Status } = require('../db.js')
 class ParcelController extends BaseController {
   // Constructor
   constructor() {
-    super(Parcel)
-
-    // @desc Get All Parcels
-    this.all = (req, res, next) => {
-      super.all(req, res, next, {
+    super(Parcel, {
+      find: {
         include: {
           model: ParcelEvent,
           as: 'events',
@@ -18,22 +15,8 @@ class ParcelController extends BaseController {
           },
         },
         order: [['id'], ['events', 'datetime', 'DESC']],
-      })
-    }
-    // @desc Get A Single Parcel
-    this.find = (req, res, next) => {
-      super.find(req, res, next, {
-        include: {
-          model: ParcelEvent,
-          as: 'events',
-          include: {
-            model: Status,
-            as: 'status',
-          },
-        },
-        order: [['id'], ['events', 'datetime', 'DESC']],
-      })
-    }
+      },
+    })
   }
 }
 
